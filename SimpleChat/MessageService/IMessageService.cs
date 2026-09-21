80b841a0e84b57caa00097f05be4c7d0ea6c808e
BaranovAA <Abaranov@integra.ru>
2026-09-21 16:44:36 +0700

    Сделал основную архитектуру

--- SimpleChat/MessageService/IMessageService.cs
+++ SimpleChat/MessageService/IMessageService.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace SimpleChat.MessageService
 {
+    // Для внутреннего использования IConnector, не использовать IConnector напрямую
     internal interface IMessageService
     {
+        void SendMessage(string message);
+
+        Task SendMessageAsync(string message);        
     }
 }
